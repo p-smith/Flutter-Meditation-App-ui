f4f90e157a8d1ec5672feafdcd1074ca377ac557
p-smith <p-smith@users.noreply.github.com>
2024-05-03 15:35:51 +0200

    Upgrade to latest flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide SearchBar;
 import 'package:flutter_svg/svg.dart';
 import 'package:meditation_app/constants.dart';
 import 'package:meditation_app/screens/details_screen.dart';
